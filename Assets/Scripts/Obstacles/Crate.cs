dd8f7bbae1335489ec0a957fc578a2a164cd1aab
xjeroen <jeroen.c.bongers@gmail.com>
2015-09-10 15:35:51 +0200

    Added gradual speed increase, and new sound.

--- Assets/Scripts/Obstacles/Crate.cs
+++ Assets/Scripts/Obstacles/Crate.cs
@@ -4,7 +4,7 @@ public class Crate : ObstacleBase
 {
     public Crate() : base(Obstacles.Riding, 1f, "Crate", new SpawnData(
         false, 1.0f, 16.5f,
-        false, 1.0f, 0.0f,
+        false, 1.0f, -0.269f,
         false, 1.0f, 0.5f))
     { }
 }
--- Assets/Scripts/Obstacles/TNT.cs
+++ Assets/Scripts/Obstacles/TNT.cs
@@ -4,7 +4,7 @@ public class TNT : ObstacleBase
 {
     public TNT() : base(Obstacles.Riding, 1f, "TNT", new SpawnData(
         false, 1.0f, 16.5f,
-        false, 1.0f, 0.0f,
+        false, 1.0f, -0.272f,
         false, 1.0f, 0.5f))
     { }
 }
